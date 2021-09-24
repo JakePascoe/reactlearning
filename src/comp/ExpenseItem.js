cfaacbeff8a112e3dfe1c25f04ed6846add280ac
Jake Pascoe <jake.pascoe@nhs.net>
2021-09-24 09:58:30 +0100

    add Json for dates, title, and amount

--- src/comp/ExpenseItem.js
+++ src/comp/ExpenseItem.js
@@ -1,15 +1,19 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const expenseDate = new Date().toLocaleDateString();
-  const expenseTitle = "Car Insurance";
-  const expenseAmout = props.amount;
+  const day = props.date.toLocaleString("en-GB", { day: "numeric" });
+  const month = props.date.toLocaleString("en-GB", { month: "short" });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{expenseDate}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">£{expenseAmout}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">£{props.amount}</div>
       </div>
     </div>
   );
